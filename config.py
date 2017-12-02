c66b6ea89181d4277d26b8f3d234b9aa27c10f63
RonistoneJunior <ronistonejunior@gmail.com>
2017-12-02 02:19:35 -0200

    add

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 DEBUG=True
 MONGO_DBNAME='hacking'
-MONG_URI='mongodb://localhost:27017/hacking'
+MONG_URI='mongodb://firedevs:qFSg62Re9MEPjvo2@hacking-shard-00-00-ixnng.mongodb.net:27017,hacking-shard-00-01-ixnng.mongodb.net:27017,hacking-shard-00-02-ixnng.mongodb.net:27017/test?ssl=true&replicaSet=hacking-shard-0&authSource=admin'
+PROJECT_ID='hackingthefire'
+DATA_BACKEND='mongodb'
+
+PASSWORD='qFSg62Re9MEPjvo2'
